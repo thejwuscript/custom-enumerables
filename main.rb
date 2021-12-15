d12adb15e61e33f1b5109ce7f3a8b30179cc29df
James Wu <ichangjwu@outlook.com>
2021-12-15 19:58:33 +0900

    reworked code for each and select

--- main.rb
+++ main.rb
@@ -1,1 +1,34 @@
-require './module.rb'
+# frozen_string_literal: true
+
+# custom enumerables
+module Enumerable
+  def my_each
+    self.length.times do |i|
+      yield(self[i])
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < self.length do
+      yield(self[i], i)
+      i += 1
+    end
+  end
+
+  def my_select
+    array = []
+    my_each  { |num| array << num if yield(num) == true}
+    array
+  end
+
+  def my_all?(argument = true)
+    if block_given?
+      # code
+    end
+    my_each do |element| 
+      return false unless element
+    end
+    true
+  end
+end
