483dd0c7f2472d0a523e171b8b5fff803180e05d
James Wu <ichangjwu@outlook.com>
2021-12-16 02:20:29 +0900

    added my_inject

--- main.rb
+++ main.rb
@@ -44,6 +44,7 @@ module Enumerable
     false
   end
 
+  # With no block given and no argument, returns true if self has no truthy elements, false otherwise.
   def my_none?(*args)
     if block_given?
       my_each { |item| return false if yield(item) == true}
@@ -58,10 +59,10 @@ module Enumerable
   def my_count(*args)
     counter = 0
     if block_given? && args.my_any?
-      puts 'warning: given block not used'
+      puts "warning: given block not used"
       my_each { |item| counter += 1 if item == args[0] }
     elsif block_given?
-      my_each { |item| counter += 1 if yield(item) == true }
+       my_each { |item| counter += 1 if yield(item) == true }
     elsif args.empty?
       return length
     else
@@ -74,5 +75,13 @@ module Enumerable
     array = []
     my_each { |item| array << yield(item) }
     array
+  end
+
+  def my_inject(*args)
+    accumulator = to_a[0]
+    self.to_a[1..-1].my_each do |item| 
+      accumulator = item.send args[0], accumulator
+    end
+    accumulator
   end
 end
